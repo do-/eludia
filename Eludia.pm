272e61704e1d1c365aa6ccc31323fc81dcaae9c9
roman <roman@430c9a1f-3e2d-0410-8b9a-efe3048821f3>
2007-06-27 07:46:31 +0000

    Обратно вернул Net::SMTP
    git-svn-id: http://svn.eludia.ru/eludia/eludia_perl/trunk/core@283 430c9a1f-3e2d-0410-8b9a-efe3048821f3

--- Eludia.pm
+++ Eludia.pm
@@ -16,6 +16,7 @@ use Number::Format;
 use Time::HiRes 'time';
 use URI::Escape;
 use Storable;
+use Net::SMTP;
 #use Math::FixedPrecision;
 
 use constant MP2 => ( exists $ENV{MOD_PERL_API_VERSION} and 
