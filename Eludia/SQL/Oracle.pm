fbb42433117afb020f1409a41693bc9f95d62042
vs <vs@430c9a1f-3e2d-0410-8b9a-efe3048821f3>
2007-11-16 09:23:05 +0000

    добавлена sql_select_ids()
    git-svn-id: http://svn.eludia.ru/eludia/eludia_perl/trunk/core@513 430c9a1f-3e2d-0410-8b9a-efe3048821f3

--- Eludia/SQL/Oracle.pm
+++ Eludia/SQL/Oracle.pm
@@ -1076,4 +1076,19 @@ return $sql;
 
 }
 
+################################################################################
+
+sub sql_select_ids {
+	my ($sql, @params) = @_;
+
+	my @ids = grep {$_ > 0} sql_select_col ($sql, @params);
+	push @ids, -1;
+
+	foreach my $parameter (@params) {
+		$sql =~ s/\?/'$parameter'/ism;
+	}
+
+	return wantarray ? (join(',', @ids), $sql) : join(',', @ids);
+}
+
 
