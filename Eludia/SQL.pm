89088d5bc0ecf868a2f9d070b608decc4169fdf0
groov <groov@430c9a1f-3e2d-0410-8b9a-efe3048821f3>
2008-05-21 13:59:27 +0000

    no_edit to sql_do_relink
    git-svn-id: http://svn.eludia.ru/eludia/eludia_perl/trunk/core@1697 430c9a1f-3e2d-0410-8b9a-efe3048821f3

--- Eludia/SQL.pm
+++ Eludia/SQL.pm
@@ -743,7 +743,7 @@ sub sql_select_id {
 
 sub sql_do_relink {
 
-	my ($table_name, $old_ids, $new_id) = @_;
+	my ($table_name, $old_ids, $new_id, $no_update) = @_;
 	
 	sql_weave_model ($DB_MODEL);
 
@@ -758,6 +758,7 @@ sub sql_do_relink {
 	my $record = sql_select_hash ($table_name, $new_id);
 	my @empty_fields = ();
 	foreach my $key (keys %$record) {
+		next if $no_update;
 		next if $record -> {$key} . '' ne '';
 		next if $key eq 'id';
 		next if $key eq 'fake';
