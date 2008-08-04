e7ce08880dacfc89e324d24799890507d82f0761
do <do@430c9a1f-3e2d-0410-8b9a-efe3048821f3>
2008-08-04 23:26:22 +0000

    - minor js optimisation
    git-svn-id: http://svn.eludia.ru/eludia/eludia_perl/trunk/core@1986 430c9a1f-3e2d-0410-8b9a-efe3048821f3

--- Eludia/Presentation/Skins/IsUp/navigation.js.pm
+++ Eludia/Presentation/Skins/IsUp/navigation.js.pm
@@ -852,39 +852,6 @@ function cell_off () {
 	return cell;	
 }
 
-
-
-
-
-function get_msword_object () {
-
-	var word;
-
-	try {
-		word = GetObject ('', 'Word.Application');
-	} catch (e) {
-		word = new ActiveXObject ('Word.Application');
-	}
-
-	word.Visible = 1;
-
-	if (word.Documents.Count == 0) {
-		word.Documents.Add ();
-	}
-		
-	return word;
-
-}
-
-function msword_line (s) {
-	
-	ms_word.Selection.InsertAfter (s); 
-	ms_word.Selection.Start = ms_word.Selection.End; 
-	ms_word.Selection.InsertParagraph (); 
-	ms_word.Selection.Start = ms_word.Selection.End; 
-
-}
-
 function actual_table_height (table, min_height, height, id_toolbar) {
 
 	var real_height       = table.firstChild.offsetHeight;
@@ -911,68 +878,6 @@ function actual_table_height (table, min_height, height, id_toolbar) {
 	return height;
 	      	
 }
-
-function image_selected(dummy_sid, id, path, width, height, image_name) {
-	if (window != opener) {
-		if (image_name=="")
-		{
-			opener.insertImageInDoc(path, width, height)
-			self.focus();
-			self.close();
-		} else {
-			opener.insertImage(id, path, width, height, image_name)
-			self.focus();
-			self.close();
-		}
-	}
-}
-
-function insertImage(id, path, width, height, image_name) {
-	if(typeof(path)=="string") {
-		id_image = eval('document.forms[0]._'+image_name);
-		id_image.value=id;
-		image_preview = eval('document.forms[0].'+image_name+'_preview')
-		image_preview.src=path;
-		image_preview.width=width;
-		image_preview.height=height;
-	}
-}
-
-function new_file_name() {
-	if (document.forms[0]._file.value!='') {
-		document.forms[0].preview.style.display='';
-	}
-	else {
-		document.forms[0].preview.style.display='none';
-		document.forms[0]._width.value='';
-		document.forms[0]._height.value='';
-	}
-	document.forms[0].preview.src=document.forms[0]._file.value;
-//		hiddenimg.src=document.imageupload.imagefile.value;
-}
-
-function show_size(obj) {
-	document.forms[0]._width.value=obj.width;
-	document.forms[0]._height.value=obj.height;
-	var W=obj.width, H=obj.height;
-	if(W>640)
-	{
-		H=H*((100.0)/W);
-		W=100;
-	}
-	
-	if(H>480)
-	{
-		W=W*((100.0)/H);
-		H=100;
-	}
-
-
-	document.forms[0].preview.width=W;
-	document.forms[0].preview.height=H;
-
-}
-
 
 /*  Copyright Mihai Bazon, 2002, 2003  |  http://dynarch.com/mishoo/
  * ------------------------------------------------------------------
