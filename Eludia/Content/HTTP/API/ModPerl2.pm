b74e1c0071696a09f48b776427240c6799115449
do <do@430c9a1f-3e2d-0410-8b9a-efe3048821f3>
2009-02-16 07:29:46 +0000

    - ModPerl2 backend fixed (Apache -> push_handlers);
    git-svn-id: http://svn.eludia.ru/eludia/eludia_perl/trunk/core@2650 430c9a1f-3e2d-0410-8b9a-efe3048821f3

--- Eludia/Content/HTTP/API/ModPerl2.pm
+++ Eludia/Content/HTTP/API/ModPerl2.pm
@@ -38,10 +38,10 @@ BEGIN {
 
 	$ENV {PERL_JSON_BACKEND} = 'JSON::PP';		
 
-	print STDERR "mod_perl 2.x, ok.\n";
+	Apache -> push_handlers (PerlChildInitHandler => \&sql_reconnect );
+	Apache -> push_handlers (PerlChildExitHandler => \&sql_disconnect);
 
-	Apache2 -> push_handlers (PerlChildInitHandler => \&sql_reconnect );
-	Apache2 -> push_handlers (PerlChildExitHandler => \&sql_disconnect);
+	print STDERR "mod_perl 2.x, ok.\n";
 
 }
 
