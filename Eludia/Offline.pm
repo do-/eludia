da9e33598dfd136fa9128ca363e10382e86f7545
do <do@eludia.ru>
2009-03-15 02:32:50 +0300

    - ::Offline is almost completely rewritten

--- Eludia/Offline.pm
+++ Eludia/Offline.pm
@@ -1,77 +1,78 @@
-BEGIN {
+no warnings;
+
+################################################################################
+
+sub config_file () {
 
 	use File::Spec;
-#	use DBIx::ModelUpdate;
-
-	$| = 1;
-
-	no warnings;
 
 	my $fn = File::Spec -> rel2abs ($0);
-
+	
 	$fn = readlink $fn while -l $fn;
-	$fn =~ s{/lib/.*}{};
-	$fn =~ s{\\lib\\.*}{};
 	
-	my $package = -e $fn . '/lib/' . __PACKAGE__ ? __PACKAGE__ : '';
+	$fn =~ y{\\}{/};
+	
+	$fn =~ s{/lib/.*}{/conf/httpd.conf};
+	
+	return $fn;
+	
+}
 
-	$PACKAGE_ROOT = [$fn . '/lib/' . $package . '/'];
-	
-	my $config_path = $fn . '/lib/' . $package . '/Config.pm';
-	
-	
-	push @INC, $PACKAGE_ROOT;
-	
-	$fn .= '/conf/httpd.conf';
+################################################################################
+
+sub perl_section_from ($) {
+
+	my ($fn) = @_;
+
+	my $code = '';
+	my $flag = 0;
 
 	open (CONF, $fn) or die ("Can't open $fn:$!\n");
-	my $conf = join '', (<CONF>);
+
+	while (<CONF>) {
+	
+		if (/<[Pp]erl\s*>/)      { $flag = 1   }
+
+		elsif (/<\/[Pp]erl\s*>/) { $flag = 0   }
+
+		elsif ($flag)            { $code .= $_ }
+	
+	}
+
 	close (CONF);
-
-	$conf =~ s{.*<[Pp]erl\s*>}{}gsm;
-	$conf =~ s{</[Pp]erl\s*>.*}{}gsm;
 	
-	eval "\$^W = 0; $conf";
-	
-	die $@ if $@;
-	
-	require Eludia;
-	
-	sql_reconnect ();
-	
-	do $config_path;
-
-	$package = __PACKAGE__;
-	eval "\$conf = \$${package}::conf";
-
-  $conf -> {systables} ||= {
-      __voc_replacements      => '__voc_replacements',
-      __access_log            => '__access_log',
-      __benchmarks            => '__benchmarks',
-      __last_update           => '__last_update',
-      __moved_links           => '__moved_links',
-      __required_files        => '__required_files',
-      __screenshots           => '__screenshots',
-      __queries               => '__queries',
-      cache_html              => 'cache_html',
-      log                     => 'log',
-      roles                   => 'roles',
-      sessions                => 'sessions',
-      users                   => 'users',
-  };
-
-	our $number_format = Number::Format -> new (%{$conf -> {number_format}});
-
-	$_REQUEST {__skin} ||= 'STDERR';
-	
-	setup_skin ();
+	return $code;
 
 }
 
+################################################################################
+
+BEGIN {
+
+	$| = 1;
+	
+	my $package = __PACKAGE__;
+
+	my $code = (perl_section_from config_file) . qq {
+	
+		package $package;
+
+		\$_REQUEST {__skin} = 'STDERR';
+
+		sql_reconnect ();
+
+	};
+	
+	eval $code;
+	
+	die $@ if $@;
+
+}
+
+################################################################################
+
 END {
-
 	sql_disconnect ();
-
 }
 
 1;
