3ef0fa51169a583255123038baf3f645a28ddd57
pik <pik@cheby.ru>
2009-03-25 15:51:11 +0300

    - __access_log table come back with $conf -> {core_session_access_logs_dbtable}

--- Eludia/Content/SessionAccessLogs.pm
+++ Eludia/Content/SessionAccessLogs.pm
@@ -1,162 +1,21 @@
-################################################################################
-
-sub session_access_log_directory () {
-
-	"$preconf->{_}->{docroot}session_access_logs";
-
-}
 
 ################################################################################
 
-sub session_access_log_filename () {
+BEGIN {
 
-	session_access_log_directory . "/$_REQUEST{sid}.4k";
-
-}
-
-################################################################################
-
-sub session_access_logs_purge {
+	print STDERR " SessionAccessLogs.....................................";
 	
-	my %session = ();
-	
-	my $directory = session_access_log_directory;
+	if ($conf -> {core_session_access_logs_dbtable}) {
 
-	opendir (DIR, $directory) or die "Can't opendir $directory:$!\n";
+		require Eludia::Content::SessionAccessLogs::DBTable;
+		print STDERR "DBTable, ok.\n";
 
-	while (my $file_name = readdir (DIR)) { 
+	} else {
 
-		$file_name =~ /\.4k$/ and $session {$`} = \$directory;
-		
+		require Eludia::Content::SessionAccessLogs::File4k;
+		print STDERR "DBTable, ok.\n";
+
 	}
-	
-	closedir (DIR);
-	
-	sql_select_loop ('SELECT id FROM sessions', sub {
-	
-		delete $session {$i -> {id}};
-		
-	});
-	
-	foreach (keys %session) { eval {
-	
-		unlink "$directory/$_.4k";
-		
-	}}
-	
-}
-
-################################################################################
-
-sub session_access_log_get {
-	
-	my $buf;
-	
-	open (F, session_access_log_filename) or return undef;
-	
-	seek (F, ($_[0] - 1) << 12, 0);
-	
-	read (F, $buf, 4096);
-
-	close (F);
-	
-	return $buf =~ / / ? $` : $buf;
-
-}
-
-################################################################################
-
-sub session_access_log_append {
-
-	my ($href) = @_;
-
-	my $filename = session_access_log_filename;
-	
-	open (F, ">>$filename") or die "Can't append to $filename:$!";
-	
-	print F $href;
-	
-	my $no = (tell F) >> 12;
-	
-	close (F);
-	
-	return $no;
-
-}
-
-################################################################################
-
-sub session_access_log_set {
-
-	my ($href) = @_;
-
-	$href =~ s{^https?\://}{};
-	
-	if ($href =~ /[\/\?]/) {
-		
-		$href = $& . $';
-	
-	}
-		
-	foreach my $key (qw(
-		_salt
-		salt
-		sid
-		id___query
-		__next_query_string
-	)) {
-	
-		$href =~ s{\&?${key}=[\d\.]+}{}g;
-	
-	}
-	
-	$href = substr $href, 0, 4095;
-
-	$href .= (' ' x (4095 - length $href));
-	
-	$href .= "\015";
-	
-	my $filename = session_access_log_filename;
-	
-	-f $filename or return session_access_log_append ($href);
-	
-	my $size = -s $filename;
-	
-	if ($size % 4096) {
-	
-		unlink $filename;
-		
-		return session_access_log_append ($href);		
-	
-	}
-	
-	my $max = $size >> 12;
-	
-	my $min = $max > 16 ? $max - 16 : 1;
-	
-	my $n;
-	
-	my $buf;
-	
-	open (F, "$filename") or die "Can't read $filename:$!";
-
-	for (my $i = $max; $i >= $min; $i --) {
-	
-		seek (F, ($i - 1) << 12, 0);
-
-		read (F, $buf, 4096);
-		
-		$buf eq $href or next;
-		
-		$n = $i;
-		
-		last;
-	
-	}
-	
-	close (F);
-	
-	return $n ? $n : session_access_log_append ($href);
 
 }
 
---
+++ Eludia/Content/SessionAccessLogs/DBTable.pm
@@ -0,0 +1,66 @@
+
+################################################################################
+
+sub session_access_logs_purge {
+	
+	my $ids = sql_select_ids ("SELECT id FROM $conf->{systables}->{sessions}");
+	
+	sql_do ("DELETE FROM $conf->{systables}->{__access_log} WHERE id_session NOT IN ($ids)");
+	
+}
+
+################################################################################
+
+sub session_access_log_get {
+	
+	sql_select_scalar ("SELECT href FROM $conf->{systables}->{__access_log} WHERE id_session = ? AND no = ?", $_REQUEST {sid}, $_ [0]);
+	
+}
+
+################################################################################
+
+sub session_access_log_append {
+
+	my ($href) = @_;
+	
+	my $no = 1 + sql_select_scalar ("SELECT MAX(no) FROM $conf->{systables}->{__access_log} WHERE id_session = ?", $_REQUEST {sid});
+	
+	sql_do ("INSERT INTO $conf->{systables}->{__access_log} (id_session, no, href) VALUES (?, ?, ?)", $_REQUEST {sid}, $no, $href);
+
+	return $no;
+
+}
+
+################################################################################
+
+sub session_access_log_set {
+
+	my ($href) = @_;
+
+	$href =~ s{^https?\://}{};
+	
+	if ($href =~ /[\/\?]/) {
+		
+		$href = $& . $';
+	
+	}
+		
+	foreach my $key (qw(
+		_salt
+		salt
+		sid
+		id___query
+		__next_query_string
+	)) {
+	
+		$href =~ s{\&?${key}=[\d\.]+}{}g;
+	
+	}
+	
+	my $no = sql_select_scalar ("SELECT no FROM $conf->{systables}->{__access_log} WHERE id_session = ? AND href LIKE ?", $_REQUEST {sid}, $href);
+		
+	return $n ? $n : session_access_log_append ($href);
+
+}
+
+1;
