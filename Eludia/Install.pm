434175959e99a8c4ee3b01b8a9082a76551eae61
Dmitry Ovsyanko <do@eludia.ru>
2009-04-07 18:15:58 +0400

    Eludia::Install::CPAN introduced

--- Eludia/Install.pm
+++ Eludia/Install.pm
@@ -18,6 +18,31 @@ $SIG {__DIE__} = \&Carp::confess;
 
 ################################################################################
 
+sub cpan {
+
+	eval 'require CPAN';
+	
+	die $@ if $@;
+	
+	foreach my $module (
+		'CGI::Simple',
+		'Data::Dumper',
+		'Digest::MD5',
+		'HTML::Parser',
+		'JSON',
+		'LWP',
+		'MIME::Base64',		
+		'Net::SMTP',
+		'Number::Format',
+		'Time::HiRes',
+		'Storable',
+		'XML::Simple',
+	) {CPAN::upgrade ($module)};
+
+}
+
+################################################################################
+
 sub decode_entities {
 
 	require HTML::Entities;
