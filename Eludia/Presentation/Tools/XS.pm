fced383648bcfb45ad356b2e5cd9bdbc5545539d
Dmitry Ovsyanko <do@eludia.ru>
2009-04-08 10:37:43 +0400

    forgotten new files

---
+++ Eludia/Presentation/Tools/XS.pm
@@ -0,0 +1,31 @@
+################################################################################
+
+sub dump_attributes {
+
+	return HTML::GenerateUtil::generate_attributes (@_);
+
+}
+
+################################################################################
+
+sub dump_tag {
+
+	return HTML::GenerateUtil::generate_tag ($_[0], $_[1], $_[2] || '', 0);
+
+}
+
+################################################################################
+
+BEGIN {
+
+	eval 'require HTML::GenerateUtil';
+	
+	return if $@;
+	
+	$preconf -> {_} -> {presentation_tools} = 'XS';
+		
+	print STDERR "HTML::GenerateUtil, ok.\n";
+	
+}
+
+1;
