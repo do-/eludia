0ac74e3a33c785dff902cb221494e7175eac696d
Dmitry Ovsyanko <do@eludia.ru>
2009-08-18 08:40:55 +0400

    MySQL/Wish/table_columns: DEFAULT issues

--- Eludia/SQL/Dialect/MySQL/Wish/table_columns.pm
+++ Eludia/SQL/Dialect/MySQL/Wish/table_columns.pm
@@ -18,7 +18,7 @@ sub wish_to_clarify_demands_for_table_columns {
 	
 	exists $i -> {NULLABLE} or $i -> {NULLABLE} = $i -> {name} eq 'id' ? 0 : 1;
 
-	$i -> {COLUMN_DEF} ||= undef;
+	exists $i -> {COLUMN_DEF} or $i -> {COLUMN_DEF} = undef;
 
 	$i -> {TYPE_NAME} = uc $i -> {TYPE_NAME};
 	
@@ -144,7 +144,7 @@ sub __genereate_sql_fragment_for_column {
 
 	}
 
-	if ($i -> {COLUMN_DEF}) {
+	if (defined $i -> {COLUMN_DEF}) {
 	
 		$i -> {COLUMN_DEF} =~ s{'}{''}g; #';
 
