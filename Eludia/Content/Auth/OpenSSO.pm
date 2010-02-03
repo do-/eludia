3e2f52b3a060725c876dd651d0ae06b925836a3f
Dmitry Ovsyanko <Dmitry.Ovsyanko@mts.ru>
2010-02-03 17:47:29 +0300

    OpenSSO fix: uri_escape for 'goto' URL

--- Eludia/Content/Auth/OpenSSO.pm
+++ Eludia/Content/Auth/OpenSSO.pm
@@ -76,7 +76,7 @@ warn Dumper ($token);
 			
 		}
 
-		$r -> headers_out -> {'Location'}   = $preconf -> {ldap} -> {opensso} . "/UI/Login?goto=http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}";
+		$r -> headers_out -> {'Location'}   = $preconf -> {ldap} -> {opensso} . "/UI/Login?goto=" . uri_escape ("http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}");
 		
 		send_http_header ();
 		
