25140103d510826f13fd13366311eb1f6a25526c
Dmitry Ovsyanko <Dmitry.Ovsyanko@mts.ru>
2010-03-05 10:11:40 +0300

    draw_[error|redirect]_page are moved away from JS.pm

--- Eludia/Presentation/Skins/Generic.pm
+++ Eludia/Presentation/Skins/Generic.pm
@@ -161,4 +161,70 @@ sub __adjust_button_href {
 
 }
 
+################################################################################
+
+sub draw_error_page {
+
+	my ($_SKIN, $page) = @_;
+
+	$_REQUEST {__content_type} ||= 'text/html; charset=' . $i18n -> {_charset};
+
+	my $data = $_JSON -> encode ([$_REQUEST {error}]);
+
+	$_REQUEST {__script} = <<EOJ;
+		function on_load () {
+EOJ
+
+	if ($page -> {error_field}) {
+		$_REQUEST{__script} .= <<EOJ;
+			var e = window.parent.document.getElementsByName('$page->{error_field}'); 
+			if (e && e[0]) { try {e[0].focus ()} catch (e) {} }				
+EOJ
+	}
+	
+	$_REQUEST {__no_back} or $_REQUEST {__script} .= "\n if (window.name != 'invisible') {history.go (-1)}\n";
+								
+	$_REQUEST {__script} .= <<EOJ;
+			var data = $data;
+			alert (data [0]);
+			try {window.top.setCursor ()} catch (e) {}
+			window.parent.document.body.style.cursor = 'default';
+		}
+EOJ
+
+	return qq{<html><head><script>$_REQUEST{__script}</script></head><body onLoad="on_load ()"></body></html>};
+
+}
+
+################################################################################
+
+sub draw_redirect_page {
+
+	my ($_SKIN, $options) = @_;
+
+	my $target = 
+		$options -> {target} ? "'$$options{target}'" : 
+		"(window.name == 'invisible' ? '_parent' : '_self')";
+
+	if ($options -> {label}) {
+		my $data = $_JSON -> encode ([$options -> {label}]);
+		$options -> {before} = "var data = $data; alert(data[0]); ";
+	}
+	
+	$options -> {$_} ||= '' foreach qw (before window_options);
+
+	return <<EOH;
+<html>
+	<script for=window event=onload>
+		$options->{before};
+		var w = window; 
+		w.open ('$options->{url}&salt=' + Math.random (), $target, '$options->{window_options}');
+	</script>
+	<body>
+	</body>
+</html>
+EOH
+
+}
+
 1;
