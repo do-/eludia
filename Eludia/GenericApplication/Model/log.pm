a740351e7cac1793c64dc16ea0cbccd40bcc66df
Pavel Koudriavtsev <root@cheby.ru>
2010-04-02 15:28:01 +0400

    log table: params field fixed

--- Eludia/GenericApplication/Model/log.pm
+++ Eludia/GenericApplication/Model/log.pm
@@ -1,21 +1,24 @@
 columns => {
 
-	dt		=> {TYPE_NAME => 'timestamp'},
+	dt          => {TYPE_NAME => 'timestamp'},
 
-	id_user		=> {TYPE_NAME => 'int'},
-	ip		=> {TYPE_NAME => 'varchar', COLUMN_SIZE => 255},
-	ip_fw		=> {TYPE_NAME => 'varchar', COLUMN_SIZE => 255},
-	action		=> {TYPE_NAME => 'varchar', COLUMN_SIZE => 255},
-	params		=> {TYPE_NAME => 'longtext'},
-	error		=> {TYPE_NAME => 'longtext'},
+	id_user     => {TYPE_NAME => 'int'},
+	ip          => {TYPE_NAME => 'varchar', COLUMN_SIZE => 255},
+	ip_fw       => {TYPE_NAME => 'varchar', COLUMN_SIZE => 255},
+	action      => {TYPE_NAME => 'varchar', COLUMN_SIZE => 255},
+	params      => {TYPE_NAME => 'longtext'},
+	error       => {TYPE_NAME => 'longtext'},
 
-	id_object	=> {TYPE_NAME => 'int',                         off => $preconf -> {_} -> {core_log} -> {version} ne 'v1'},
-	type		=> {TYPE_NAME => 'varchar', COLUMN_SIZE => 255, off => $preconf -> {_} -> {core_log} -> {version} ne 'v1'},
-	params		=> {TYPE_NAME => 'longtext',                    off => $preconf -> {_} -> {core_log} -> {version} ne 'v1'},
+	id_object   => {TYPE_NAME => 'int',                         off => $preconf -> {_} -> {core_log} -> {version} ne 'v1'},
+	type        => {TYPE_NAME => 'varchar', COLUMN_SIZE => 255, off => $preconf -> {_} -> {core_log} -> {version} ne 'v1'},
 
-	href		=> {TYPE_NAME => 'varchar', COLUMN_SIZE => 255, off => $preconf -> {_} -> {core_log} -> {version} ne 'v2'},
-	params		=> {TYPE_NAME => 'text',                        off => $preconf -> {_} -> {core_log} -> {version} ne 'v2'},
+	params      => {
+		TYPE_NAME => $preconf -> {_} -> {core_log} -> {version} eq 'v1' ? 'longtext' : 'text',
+		off       => $preconf -> {_} -> {core_log} -> {version} ne 'v1' && $preconf -> {_} -> {core_log} -> {version} ne 'v2',
+	},
 
-	mac		=> {TYPE_NAME => 'varchar', COLUMN_SIZE => 17,  off => $preconf -> {_} -> {core_log} -> {log_mac} == 0},
+	href        => {TYPE_NAME => 'varchar', COLUMN_SIZE => 255, off => $preconf -> {_} -> {core_log} -> {version} ne 'v2'},
+
+	mac         => {TYPE_NAME => 'varchar', COLUMN_SIZE => 17,  off => $preconf -> {_} -> {core_log} -> {log_mac} == 0},
 
 },
