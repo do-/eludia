do-/eludia

commit d3d953c819b29046e5d14864147af4e5789685b8

TurboMilk::draw_toolbar_input_text: don't submit twice

Eludia/Presentation/Skins/TurboMilk.pm
@@ -1949,7 +1949,7 @@ sub draw_toolbar_input_text {
 
 	$html .= <<EOH;
 		<input 
-			onKeyPress="$$options{onKeyPress}" 
+			onKeyPress="$$options{onKeyPress}; blockEvent ();" 
 			type=text 
 			size=$$options{size} 
 			name=$$options{name} 

commit 07419016761cecbf7d77c1197de1fee2195952c9

TurboMilk::draw_toolbar_input_text: kb support was broken since d3d953c

Eludia/Presentation/Skins/TurboMilk.pm
@@ -1943,13 +1943,13 @@ sub draw_toolbar_input_text {
 	$options -> {attributes} ||= {};
 	$options -> {attributes} -> {style} ||= 'visibility:expression(select_visibility())' if msie_less_7;
 	
-	$options -> {onKeyPress} ||= "if (window.event.keyCode == 13) {form.submit()}";
+	$options -> {onKeyPress} ||= "if (window.event.keyCode == 13) {form.submit (); blockEvent ()}";
 
 	my $attributes = dump_attributes ($options -> {attributes});
 
 	$html .= <<EOH;
 		<input 
-			onKeyPress="$$options{onKeyPress}; blockEvent ();" 
+			onKeyPress="$$options{onKeyPress};" 
 			type=text 
 			size=$$options{size} 
 			name=$$options{name} 