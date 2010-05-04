67986c135b008bc1c0a9675e70c07497725d4b86
Dmitry Ovsyanko <do@eludia.ru>
2010-05-04 16:49:04 +0400

    Auth: if all pre_auth hooks fail... OK, let it be.

--- Eludia/Content/Auth.pm
+++ Eludia/Content/Auth.pm
@@ -112,7 +112,7 @@ sub get_user_with_fixed_session {
 
 sub get_user {
 
-	foreach (@{$preconf -> {_} -> {pre_auth}}) {&$_ ()};
+	eval { foreach (@{$preconf -> {_} -> {pre_auth}}) {&$_ ()} }; warn $@ if $@;
 	
 	my $user = get_user_with_fixed_session (check_peer_server ());
 	
