d5fa7a489500498eafa298b05913307829bc7ae0
Dmitry Ovsyanko <do@eludia.ru>
2010-10-29 11:29:03 +0400

    TinySSO: detecting protocol by port number

--- Eludia/Content/Auth/TinySSO.pm
+++ Eludia/Content/Auth/TinySSO.pm
@@ -2,7 +2,9 @@ no warnings;
 
 push @{$preconf -> {_} -> {pre_auth}}, sub {
 
-	$_REQUEST {sid} or $_REQUEST {login} or redirect ($preconf -> {ldap} -> {tinysso} . '?goto=' . uri_escape ("http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}"));
+	my $proto = $ENV {SERVER_PORT} == 443 ? 'https' : 'http';
+
+	$_REQUEST {sid} or $_REQUEST {login} or redirect ($preconf -> {ldap} -> {tinysso} . '?goto=' . uri_escape ("${proto}://$ENV{HTTP_HOST}$ENV{REQUEST_URI}"));
 		
 };
 
