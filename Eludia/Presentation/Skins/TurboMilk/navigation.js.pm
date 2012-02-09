aaeeb61743468d9fea9c276fb00d2b25adb02a9e
Pavel Koudriavtsev <pik@cheby.ru>
2012-02-09 15:27:09 +0400

    JS functions: number_format && number_clean to operate with numbers

--- Eludia/Presentation/Skins/TurboMilk/navigation.js.pm
+++ Eludia/Presentation/Skins/TurboMilk/navigation.js.pm
@@ -3176,7 +3176,50 @@ Calendar.setup = function (params) {
 
 
 
+function number_format( number, decimals, dec_point, thousands_sep ) {	// Format a number with grouped thousands
+	//
+	// +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+	// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +	 bugfix by: Michael White (http://crestidg.com)
 
+	var i, j, kw, kd, km, sign = '';
+
+	// input sanitation & defaults
+	if( isNaN(decimals = Math.abs(decimals)) ){
+		decimals = 2;
+	}
+	if( dec_point == undefined ){
+		dec_point = ",";
+	}
+	if( thousands_sep == undefined ){
+		thousands_sep = " ";
+	}
+	if (number < 0) {
+		sign = '-';
+		number = -number;
+	}
+
+	i = parseInt(number = (+number || 0).toFixed(decimals)) + "";
+
+	if( (j = i.length) > 3 ){
+		j = j % 3;
+	} else{
+		j = 0;
+	}
+
+	km = (j ? i.substr(0, j) + thousands_sep : "");
+	kw = i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep);
+	//kd = (decimals ? dec_point + Math.abs(number - i).toFixed(decimals).slice(2) : "");
+	kd = (decimals ? dec_point + Math.abs(number - i).toFixed(decimals).replace(/-/, 0).slice(2) : "");
+
+
+	return sign + km + kw + kd;
+}
+
+function number_clean (number) {
+	var result = Number ((number || "").replace(/\s+/g, '').replace(/\,/, '.'));
+	return isNaN (result) ? 0 : result;
+}
 
 
 
