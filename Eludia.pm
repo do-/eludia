7cf9889a8c82144b21242f29233a5039fd0001a3
Dmitry Ovsyanko <do_@pochta.ru>
2012-02-16 23:03:24 +0400

    (x86) may happen in doocroot

--- Eludia.pm
+++ Eludia.pm
@@ -278,7 +278,7 @@ sub check_application_directory {
 		
 		close (IN);
 		
-		if ($httpd_conf =~ /^\s*DocumentRoot\s+([\"\'\\\/\w\.\:\- ]+)/gism) {
+		if ($httpd_conf =~ /^\s*DocumentRoot\s+([\"\'\\\/\w\.\:\-\(\) ]+)/gism) {
 		
 			$docroot = $1;
 			
