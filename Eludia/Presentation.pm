do-/eludia

commit ce245f00ab5d89f253c024271604254512d23394

double quotes in filter broke filter button href

Eludia/Presentation.pm
@@ -416,6 +416,7 @@ sub check_href {
 				next if $k =~ /^_/ && !$_INHERITABLE_PARAMETER_NAMES -> {$k};
 				next if             $_NONINHERITABLE_PARAMETER_NAMES -> {$k};
 				$h {$k} = uri_escape ($_REQUEST {$k});
+				$h {$k} = encode_entities ($_REQUEST {$k}, '"');
 
 			}
 			

commit 483f6a365338456407469a2774cfeb0b5927a9ee

check_href: broken href since uri_escape skipped (bug introduced by ce245f00)

Eludia/Presentation.pm
@@ -416,7 +416,7 @@ sub check_href {
 				next if $k =~ /^_/ && !$_INHERITABLE_PARAMETER_NAMES -> {$k};
 				next if             $_NONINHERITABLE_PARAMETER_NAMES -> {$k};
 				$h {$k} = uri_escape ($_REQUEST {$k});
-				$h {$k} = encode_entities ($_REQUEST {$k}, '"');
+				$h {$k} = encode_entities ($h {$k}, '"');
 
 			}
 			