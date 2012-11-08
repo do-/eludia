b21f9e97f91c0609d274e69917ab4d36ab33f43f
Dmitry Ovsyanko <do_@pochta.ru>
2012-11-08 23:41:30 +0400

    JSONDumper: 1 more try

--- Eludia/Presentation/Skins/JSONDumper.pm
+++ Eludia/Presentation/Skins/JSONDumper.pm
@@ -14,7 +14,7 @@ sub draw_hash {
 
 	my ($_SKIN, $h) = @_;
 
-	$_REQUEST {__content_type} ||= 'text/plain; charset=' . $i18n -> {_charset};
+	$_REQUEST {__content_type} ||= 'text/plain; charset=utf8';
 
 	$_JSON -> encode ($h);
 
@@ -25,9 +25,7 @@ sub draw_hash {
 sub draw_page {
 
 	my ($_SKIN, $page) = @_;
-	
-	$page -> {content} -> {__this_content_is_ok_to_be_shown_completely} or $page -> {content} = 'Sorry?..';
-	
+		
 	return $_SKIN -> draw_hash ({ 
 	
 		content => $page -> {content},
