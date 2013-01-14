f94471083f724624860b0c7c5328bb1e51b41490
Pavel Koudriavtsev <pik@cheby.ru>
2013-01-14 16:15:31 +0400

    draw_{type}_cell - call draw_text_cell from file

--- Eludia/Presentation/TableCells/textarea.pm
+++ Eludia/Presentation/TableCells/textarea.pm
@@ -1,21 +1,22 @@
 sub draw_textarea_cell {
 
 	my ($data, $options) = @_;
-	
-	return draw_text_cell ($data, $options) if ($_REQUEST {__read_only} && !$data -> {edit}) || $data -> {read_only} || $data -> {off};
+
+	return call_from_file ('Eludia/Presentation/TableCells/text.pm', 'draw_text_cell', @_)
+		if ($_REQUEST {__read_only} && !$data -> {edit}) || $data -> {read_only} || $data -> {off};
 
 	$data -> {rows} ||= 3;
 	$data -> {cols} ||= 80;
 
 	$data -> {attributes} ||= {};
 	$data -> {attributes} -> {class} ||= 'row-cell';
-	
+
 	_adjust_row_cell_style ($data, $options);
-						
+
 	$data -> {label} ||= '';
-			
+
 	check_title ($data);
-		
+
 	return $_SKIN -> draw_textarea_cell ($data, $options);
 
 }
