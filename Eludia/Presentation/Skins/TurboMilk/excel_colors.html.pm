096a3bef7cea132c60eab03eb6e159122f71b757
Anton Kuznecov <anton@cheby.ru>
2013-03-14 14:29:34 +0400

    Задача №6424. Новая палитра "Excel2010"

---
+++ Eludia/Presentation/Skins/TurboMilk/excel_colors.html.pm
@@ -0,0 +1,123 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<HTML>
+<HEAD>
+
+	<TITLE></TITLE>
+
+	<META NAME="Generator" CONTENT="TextPad 4.6">
+	<META NAME="Author" CONTENT="?">
+	<META NAME="Keywords" CONTENT="?">
+	<META NAME="Description" CONTENT="?">
+
+	<SCRIPT LANGUAGE="JavaScript" FOR="cell" EVENT="onclick">
+
+	  window.returnValue = event.srcElement.value;
+	  window.close ();
+
+	</SCRIPT>
+
+</HEAD>
+
+<BODY BGCOLOR="#FFFFFF" TEXT="#000000" LINK="#FF0000" VLINK="#800000" ALINK="#FF00FF" BACKGROUND="?">
+
+	<table id="ColorTable" border="0" cellspacing="5" cellpadding="0" width="100%" height="20px" style="margin-bottom:5px;">
+		<tbody>
+			<tr>
+				<td id="cell" style="background-color:#FFFFFF" value="FFFFFF">&nbsp;</td>
+				<td id="cell" style="background-color:#000000" value="000000">&nbsp;</td>
+				<td id="cell" style="background-color:#EEECE1" value="EEECE1">&nbsp;</td>
+				<td id="cell" style="background-color:#1F497D" value="1F497D">&nbsp;</td>
+				<td id="cell" style="background-color:#4F81BD" value="4F81BD">&nbsp;</td>
+				<td id="cell" style="background-color:#C0504D" value="C0504D">&nbsp;</td>
+				<td id="cell" style="background-color:#9BBB59" value="9BBB59">&nbsp;</td>
+				<td id="cell" style="background-color:#8064A2" value="8064A2">&nbsp;</td>
+				<td id="cell" style="background-color:#4BACC6" value="4BACC6">&nbsp;</td>
+				<td id="cell" style="background-color:#F79646" value="F79646">&nbsp;</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<table id="ColorTable" border="0" cellspacing="5" cellpadding="0" width="100%" style="margin-bottom:5px;">
+		<tbody>
+			<tr height="20px">
+				<td id="cell" style="background-color:#F2F2F2" value="F2F2F2">&nbsp;</td>
+				<td id="cell" style="background-color:#7F7F7F" value="7F7F7F">&nbsp;</td>
+				<td id="cell" style="background-color:#DDD9C3" value="DDD9C3">&nbsp;</td>
+				<td id="cell" style="background-color:#C6D9F0" value="C6D9F0">&nbsp;</td>
+				<td id="cell" style="background-color:#DBE5F1" value="DBE5F1">&nbsp;</td>
+				<td id="cell" style="background-color:#F2DCDB" value="F2DCDB">&nbsp;</td>
+				<td id="cell" style="background-color:#EBF1DD" value="EBF1DD">&nbsp;</td>
+				<td id="cell" style="background-color:#E5E0EC" value="E5E0EC">&nbsp;</td>
+				<td id="cell" style="background-color:#DBEEF3" value="DBEEF3">&nbsp;</td>
+				<td id="cell" style="background-color:#FDEADA" value="FDEADA">&nbsp;</td>
+			</tr>
+			<tr height="20px">
+				<td id="cell" style="background-color:#D8D8D8" value="D8D8D8">&nbsp;</td>
+				<td id="cell" style="background-color:#595959" value="595959">&nbsp;</td>
+				<td id="cell" style="background-color:#C4BD97" value="C4BD97">&nbsp;</td>
+				<td id="cell" style="background-color:#8DB3E2" value="8DB3E2">&nbsp;</td>
+				<td id="cell" style="background-color:#B8CCE4" value="B8CCE4">&nbsp;</td>
+				<td id="cell" style="background-color:#E5B9B7" value="E5B9B7">&nbsp;</td>
+				<td id="cell" style="background-color:#D7E3BC" value="D7E3BC">&nbsp;</td>
+				<td id="cell" style="background-color:#CCC1D9" value="CCC1D9">&nbsp;</td>
+				<td id="cell" style="background-color:#B7DDE8" value="B7DDE8">&nbsp;</td>
+				<td id="cell" style="background-color:#FBD5B5" value="FBD5B5">&nbsp;</td>
+			</tr>
+			<tr height="20px">
+				<td id="cell" style="background-color:#BFBFBF" value="BFBFBF">&nbsp;</td>
+				<td id="cell" style="background-color:#3F3F3F" value="3F3F3F">&nbsp;</td>
+				<td id="cell" style="background-color:#938953" value="938953">&nbsp;</td>
+				<td id="cell" style="background-color:#548DD4" value="548DD4">&nbsp;</td>
+				<td id="cell" style="background-color:#95B3D7" value="95B3D7">&nbsp;</td>
+				<td id="cell" style="background-color:#D99694" value="D99694">&nbsp;</td>
+				<td id="cell" style="background-color:#C3D69B" value="C3D69B">&nbsp;</td>
+				<td id="cell" style="background-color:#B2A2C7" value="B2A2C7">&nbsp;</td>
+				<td id="cell" style="background-color:#92CDDC" value="92CDDC">&nbsp;</td>
+				<td id="cell" style="background-color:#FAC08F" value="FAC08F">&nbsp;</td>
+			</tr>
+			<tr height="20px">
+				<td id="cell" style="background-color:#A5A5A5" value="A5A5A5">&nbsp;</td>
+				<td id="cell" style="background-color:#262626" value="262626">&nbsp;</td>
+				<td id="cell" style="background-color:#494429" value="494429">&nbsp;</td>
+				<td id="cell" style="background-color:#17365D" value="17365D">&nbsp;</td>
+				<td id="cell" style="background-color:#366092" value="366092">&nbsp;</td>
+				<td id="cell" style="background-color:#953734" value="953734">&nbsp;</td>
+				<td id="cell" style="background-color:#76923C" value="76923C">&nbsp;</td>
+				<td id="cell" style="background-color:#5F497A" value="5F497A">&nbsp;</td>
+				<td id="cell" style="background-color:#31859B" value="31859B">&nbsp;</td>
+				<td id="cell" style="background-color:#E36C09" value="E36C09">&nbsp;</td>
+			</tr>
+			<tr height="20px">
+				<td id="cell" style="background-color:#7F7F7F" value="7F7F7F">&nbsp;</td>
+				<td id="cell" style="background-color:#0C0C0C" value="0C0C0C">&nbsp;</td>
+				<td id="cell" style="background-color:#1D1B10" value="1D1B10">&nbsp;</td>
+				<td id="cell" style="background-color:#0F243E" value="0F243E">&nbsp;</td>
+				<td id="cell" style="background-color:#244061" value="244061">&nbsp;</td>
+				<td id="cell" style="background-color:#632423" value="632423">&nbsp;</td>
+				<td id="cell" style="background-color:#4F6128" value="4F6128">&nbsp;</td>
+				<td id="cell" style="background-color:#3F3151" value="3F3151">&nbsp;</td>
+				<td id="cell" style="background-color:#205867" value="205867">&nbsp;</td>
+				<td id="cell" style="background-color:#974806" value="974806">&nbsp;</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<table id="ColorTable" border="0" cellspacing="5" cellpadding="0" width="100%" height="20px">
+		<tbody>
+			<tr>
+				<td id="cell" style="background-color:#C00000" value="C00000">&nbsp;</td>
+				<td id="cell" style="background-color:#FF0000" value="FF0000">&nbsp;</td>
+				<td id="cell" style="background-color:#FFC000" value="FFC000">&nbsp;</td>
+				<td id="cell" style="background-color:#FFFF00" value="FFFF00">&nbsp;</td>
+				<td id="cell" style="background-color:#92D050" value="92D050">&nbsp;</td>
+				<td id="cell" style="background-color:#00B050" value="00B050">&nbsp;</td>
+				<td id="cell" style="background-color:#00B0F0" value="00B0F0">&nbsp;</td>
+				<td id="cell" style="background-color:#0070C0" value="0070C0">&nbsp;</td>
+				<td id="cell" style="background-color:#002060" value="002060">&nbsp;</td>
+				<td id="cell" style="background-color:#7030A0" value="7030A0">&nbsp;</td>
+			</tr>
+		</tbody>
+	</table>
+
+</BODY>
+</HTML>
