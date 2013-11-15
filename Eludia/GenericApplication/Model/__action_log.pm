c6c13c5edb4a59cf68a69c2cf8be221690c365a1
Pavel Koudriavtsev <pik@cheby.ru>
2013-11-15 14:43:00 +0400

    dbl_click_protection_master

---
+++ Eludia/GenericApplication/Model/__action_log.pm
@@ -0,0 +1,14 @@
+columns => {
+	id          => {TYPE_NAME => 'bigint', _EXTRA => 'auto_increment', _PK => 1},
+	id_log      => {TYPE_NAME => 'bigint'},
+	pid         => {TYPE_NAME => 'bigint'},
+	id_session  => {TYPE_NAME => 'bigint'},
+	ts          => {TYPE_NAME => 'timestamp'},
+	params_hash => {TYPE_NAME => 'text'},
+	return_url  => {TYPE_NAME => 'text'},
+	redirect_alert => {TYPE_NAME => 'text'},
+},
+
+keys => {
+	ix => 'id_session,params_hash(255)',
+},
