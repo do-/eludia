0075dbd750e36900f245b6b567280d08ae4931a3
Maksim Gruzdev <mgruzdev@cheby.ru>
2014-03-21 15:17:48 +0400

    super_table Mint skin introduced

---
+++ Eludia/Presentation/Skins/Mint/0.html.pm
@@ -0,0 +1,7 @@
+<html>
+<head>
+	<LINK href="/i/_skins/Mint/eludia.css?7547658658" type="text/css" rel="STYLESHEET" />
+	<script src="/i/_skins/Mint/navigation.js">
+	</script>
+</head>
+<body>&nbsp;</body></html>
---
+++ Eludia/Presentation/Skins/Mint/i18n_FRE.js.pm
@@ -0,0 +1,28 @@
+var i18n = {
+	F5: "Attention! Vous avez changé le contenu de certains champs d'entrée. Rechargant la page, vous perdrez cette information. Continuer l'opération?"
+};
+
+function i18n_calendar (Calendar) {
+
+	Calendar._DN = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+	Calendar._MN = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+	Calendar._TT = {
+		"TOGGLE"          : "Changer le premier jour de la semaine",
+		"PREV_YEAR"       : "Année préc. (maintenir pour menu)",
+		"PREV_MONTH"      : "Mois préc. (maintenir pour menu)",
+		"GO_TODAY"        : "Atteindre date du jour",
+		"NEXT_MONTH"      : "Mois suiv. (maintenir pour menu)",
+		"NEXT_YEAR"       : "Année suiv. (maintenir pour menu)",
+		"SEL_DATE"        : "Choisir une date",
+		"DRAG_TO_MOVE"    : "Déplacer",
+		"PART_TODAY"      : " (Aujourd'hui)",
+		"MON_FIRST"       : "Commencer par lundi",
+		"SUN_FIRST"       : "Commencer par dimanche",
+		"CLOSE"           : "Fermer",
+		"TODAY"           : "Aujourd'hui",
+		"DEF_DATE_FORMAT" : "y-mm-dd",
+		"TT_DATE_FORMAT"  : "D, M d",
+		"WK"              : "wk"
+	};
+
+}
