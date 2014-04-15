4c6ab81aecac75125d0ccfe3824cf8786973389e
Maksim Gruzdev <mgruzdev@cheby.ru>
2014-04-15 16:36:30 +0400

    4235 draw_form_field_files: sql_upload_files works with multiple file fields
    Conflicts:
    	Eludia/SQL/Dialect/MySQL.pm

--- Eludia/SQL.pm
+++ Eludia/SQL.pm
@@ -1184,19 +1184,9 @@ sub sql_upload_files {
 
 	my ($options) = @_;
 
-	my @nos = ();
+	my $uploaded_files = upload_files ($options);
 
-	foreach my $k (keys %_REQUEST) {
-
-		$k =~ /^_$options->{name}_(\d+)$/ or next;
-
-		$_REQUEST {$k} or next;
-
-		push @nos, $1;
-
-	}
-
-	@nos > 0 or return;
+	@$uploaded_files > 0 or return;
 
 	my ($table, $field) = split /\./, $_REQUEST {"__$options->{name}_file_field"};
 
@@ -1215,9 +1205,7 @@ sub sql_upload_files {
 	$options -> {path_column}      = 'file_path';
 	$options -> {body_column}      = 'file_body' if $model_update -> get_columns ($table) -> {file_body};
 
-	foreach my $no (sort {$a <=> $b} @nos) {
-
-		$options -> {name} = "${name}_${no}";
+	foreach my $upload (@$uploaded_files) {
 
 		$options -> {id} = sql_do_insert ($table => {
 
@@ -1226,9 +1214,12 @@ sub sql_upload_files {
 
 		});
 
-		sql_upload_file ($options);
+		$options -> {upload} = $upload;
 
+		sql_upload_file ($options);
 	}
+
+	delete $options -> {upload};
 
 	sql_select_loop ("SELECT * FROM $table WHERE $field = ? AND fake = -1", sub {
 
