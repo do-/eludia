b99e33ce5792fa2e4a4c2344c568dba24d3e7e17
Pavel Koudriavtsev <pik@cheby.ru>
2014-08-25 16:28:04 +0400

    super_table & mint menu & kendo ui

--- Eludia/Presentation/Skins/Mint/eludia.css.pm
+++ Eludia/Presentation/Skins/Mint/eludia.css.pm
@@ -8,6 +8,7 @@ HTML, BODY {
 	height:100%;
 	margin:0px;
 	padding:0px;
+	overflow: auto;
 }
 
 IFRAME {
@@ -73,6 +74,10 @@ div.checkboxes {
 	padding-left: 10px;
 	float: left;
 }
+
+# .filters li .k-widget, .filters li .k-widget .k-dropzone {
+# 	display: inline;
+# }
 
 ol, ul {list-style: none;}
 
@@ -190,295 +195,7 @@ div.modal_div  > * {
 	background-image: none !important;
 }
 
-/* Calendar styles */
-
-
-/* The main calendar widget.  DIV containing a table. */
-
-.calendar {
-  position: absolute;
-  z-index: 200;
-  display: none;
-  border-top: 2px solid #fff;
-  border-right: 2px solid #000;
-  border-bottom: 2px solid #000;
-  border-left: 2px solid #fff;
-  font-size: 11px;
-  color: #000;
-  cursor: default;
-  background: #D7DFE6;
-  font-family: tahoma,verdana,sans-serif;
-}
-
-.calendar table {
-  border-top: 1px solid #000;
-  border-right: 1px solid #fff;
-  border-bottom: 1px solid #fff;
-  border-left: 1px solid #000;
-  font-size: 11px;
-  color: #000;
-  cursor: default;
-  background: #D7DFE6;
-  font-family: tahoma,verdana,sans-serif;
-}
-
-/* Header part -- contains navigation buttons and day names. */
-
-.calendar .button { /* "<<", "<", ">", ">>" buttons have this class */
-  text-align: center;
-  padding: 1px;
-  border-top: 1px solid #fff;
-  border-right: 1px solid #000;
-  border-bottom: 1px solid #000;
-  border-left: 1px solid #fff;
-}
-
-.calendar thead .title { /* This holds the current "month, year" */
-  font-weight: bold;
-  padding: 1px;
-  border: 1px solid #000;
-  background: #595F95;
-  color: #fff;
-  text-align: center;
-}
-
-.calendar thead .headrow { /* Row <TR> containing navigation buttons */
-}
-
-.calendar thead .daynames { /* Row <TR> containing the day names */
-}
-
-.calendar thead .name { /* Cells <TD> containing the day names */
-  border-bottom: 1px solid #000;
-  padding: 2px;
-  text-align: center;
-  background: #F9F9FF;
-}
-
-.calendar thead .weekend { /* How a weekend day name shows in header */
-  color: #f00;
-}
-
-.calendar thead .hilite { /* How do the buttons in header appear when hover */
-  border-top: 2px solid #fff;
-  border-right: 2px solid #000;
-  border-bottom: 2px solid #000;
-  border-left: 2px solid #fff;
-  padding: 0px;
-  background-color: #e8e5f0;
-}
-
-.calendar thead .active { /* Active (pressed) buttons in header */
-  padding: 2px 0px 0px 2px;
-  border-top: 1px solid #000;
-  border-right: 1px solid #fff;
-  border-bottom: 1px solid #fff;
-  border-left: 1px solid #000;
-  background-color: #95A3B9;
-}
-
-/* The body part -- contains all the days in month. */
-
-.calendar tbody .day { /* Cells <TD> containing month days dates */
-  width: 2em;
-  text-align: right;
-  padding: 2px 4px 2px 2px;
-}
-
-.calendar table .wn {
-  padding: 2px 3px 2px 2px;
-  border-right: 1px solid #000;
-  background: #F9F9FF;
-}
-
-.calendar tbody .rowhilite td {
-  background: #e8e5f0;
-}
-
-.calendar tbody .rowhilite td.wn {
-  background: #D7DFE6;
-}
-
-.calendar tbody td.hilite { /* Hovered cells <TD> */
-  padding: 1px 3px 1px 1px;
-  border-top: 1px solid #fff;
-  border-right: 1px solid #000;
-  border-bottom: 1px solid #000;
-  border-left: 1px solid #fff;
-}
-
-.calendar tbody td.active { /* Active (pressed) cells <TD> */
-  padding: 2px 2px 0px 2px;
-  border-top: 1px solid #000;
-  border-right: 1px solid #fff;
-  border-bottom: 1px solid #fff;
-  border-left: 1px solid #000;
-}
-
-.calendar tbody td.selected { /* Cell showing selected date */
-  font-weight: bold;
-  border-top: 1px solid #000;
-  border-right: 1px solid #fff;
-  border-bottom: 1px solid #fff;
-  border-left: 1px solid #000;
-  padding: 2px 2px 0px 2px;
-  background: #e8e5f0;
-}
-
-.calendar tbody td.weekend { /* Cells showing weekend days */
-  color: #f00;
-}
-
-.calendar tbody td.today { /* Cell showing today date */
-  font-weight: bold;
-  color: #00f;
-}
-
-.calendar tbody .disabled { color: #999; }
-
-.calendar tbody .emptycell { /* Empty cells (the best is to hide them) */
-  visibility: hidden;
-}
-
-.calendar tbody .emptyrow { /* Empty row (some months need less than 6 rows) */
-  display: none;
-}
-
-/* The footer part -- status bar and "Close" button */
-
-.calendar tfoot .footrow { /* The <TR> in footer (only one right now) */
-}
-
-.calendar tfoot .ttip { /* Tooltip (status bar) cell <TD> */
-  padding: 1px;
-  border: 1px solid #000;
-  background: #595F95;
-  color: #fff;
-  text-align: center;
-}
-
-.calendar tfoot .hilite { /* Hover style for buttons in footer */
-  border-top: 1px solid #fff;
-  border-right: 1px solid #000;
-  border-bottom: 1px solid #000;
-  border-left: 1px solid #fff;
-  padding: 1px;
-  background: #e8e5f0;
-}
-
-.calendar tfoot .active { /* Active (pressed) style for buttons in footer */
-  padding: 2px 0px 0px 2px;
-  border-top: 1px solid #000;
-  border-right: 1px solid #fff;
-  border-bottom: 1px solid #fff;
-  border-left: 1px solid #000;
-}
-
-/* Combo boxes (menus that display months/years for direct selection) */
-
-.combo {
-  position: absolute;
-  display: none;
-  width: 4em;
-  top: 0px;
-  left: 0px;
-  cursor: default;
-  border-top: 1px solid #fff;
-  border-right: 1px solid #000;
-  border-bottom: 1px solid #000;
-  border-left: 1px solid #fff;
-  background: #e8e5f0;
-  font-size: smaller;
-  padding: 1px;
-}
-
-.combo .label,
-.combo .label-IEfix {
-  text-align: center;
-  padding: 1px;
-}
-
-.combo .label-IEfix {
-  width: 4em;
-}
-
-.combo .active {
-  background: #95A3B9;
-  padding: 0px;
-  border-top: 1px solid #000;
-  border-right: 1px solid #fff;
-  border-bottom: 1px solid #fff;
-  border-left: 1px solid #000;
-}
-
-.combo .hilite {
-  background: #048;
-  color: #fea;
-}
-
-.calendar td.time {
-  border-top: 1px solid #000;
-  padding: 1px 0px;
-  text-align: center;
-  background-color: #F9F9FF;
-}
-
-.calendar td.time .hour,
-.calendar td.time .minute,
-.calendar td.time .ampm {
-  padding: 0px 3px 0px 4px;
-  border: 1px solid #889;
-  font-weight: bold;
-  background-color: #fff;
-}
-
-.calendar td.time .ampm {
-  text-align: center;
-}
-
-.calendar td.time .colon {
-  padding: 0px 2px 0px 3px;
-  font-weight: bold;
-}
-
-.calendar td.time span.hilite {
-  border-color: #000;
-  background-color: #766;
-  color: #fff;
-}
-
-.calendar td.time span.active {
-  border-color: #f00;
-  background-color: #000;
-  color: #0f0;
-}
-
-
-
-BODY {
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
-	FONT-WEIGHT: normal;
-	FONT-SIZE: 8pt;
-	COLOR: #000000;
-	background-color: #FFFFFF
-}
-
-.header_0 {
-	FONT-WEIGHT: bold;
-	FONT-SIZE: 12pt;
-	COLOR: #63a014;
-	FONT-FAMILY: 'Trebuchet MS', 'MS Sans Serif';
-}
-
-.header_1 {
-	FONT-WEIGHT: bold; FONT-SIZE: 10pt; COLOR: #2f3237; FONT-FAMILY: Tahoma, 'MS Sans Serif';
-}
-
-.header_2 {
-	FONT-WEIGHT: bold; FONT-SIZE: 8pt; COLOR: #ffffff; FONT-FAMILY: Tahoma, 'MS Sans Serif';
-}
-
-.header_3 {
+.table_title {
 	FONT-WEIGHT: bold;
 	FONT-SIZE: 10pt;
 	COLOR: #2f3237;
@@ -486,36 +203,10 @@ BODY {
 	background-color: #edf1f5;
 }
 
-.header_4 {
-	FONT-WEIGHT: normal;
-	FONT-SIZE: 25pt;
-	COLOR: #484e9d;
-	FONT-FAMILY: 'Trebuchet MS', 'MS Sans Serif';
-}
-
-.header_5 {
-	FONT-WEIGHT: normal;
-	FONT-SIZE: 16pt;
-	COLOR: #7c7c7c;
-	FONT-FAMILY: 'Trebuchet MS', 'MS Sans Serif';
-}
-
-.bgr8 {
-	FONT-SIZE: 8pt; FONT-FAMILY: Tahoma, 'MS Sans Serif';
-}
-
 A.button, A.button:link, A.button:active, A.button:hover, A.button:visited {
 	FONT-WEIGHT: normal; FONT-SIZE: 8pt; COLOR: #232324; FONT-FAMILY: Tahoma, 'MS Sans Serif'; TEXT-DECORATION: none
 }
 
-
-a.main-menu, a.main-menu:link, a.main-menu:active, a.main-menu:visited, a.main-menu:hover {
-	font-family: Tahoma, 'MS Sans Serif';
-	FONT-WEIGHT: bold;
-	font-size: 8pt;
-	color: #ffffff;
-	text-decoration: none;
-}
 
 a.tab-0, a.tab-0:link, a.tab-0:active, a.tab-0:visited, a.tab-0:hover {
 	font-family: Tahoma, 'MS Sans Serif';
@@ -529,21 +220,6 @@ a.tab-1, a.tab-1:link, a.tab-1:active, a.tab-1:visited, a.tab-1:hover {
 	text-decoration: none;
 }
 
-.txt0 {
-	FONT-WEIGHT: bold; FONT-SIZE: 8pt; COLOR: #000000; FONT-FAMILY: Tahoma, 'MS Sans Serif';
-}
-
-.txt1 {
-	FONT-WEIGHT: normal; FONT-SIZE: 8pt; COLOR: #000000; FONT-FAMILY: Tahoma, 'MS Sans Serif';
-}
-
-.txt2 {
-	FONT-WEIGHT: normal; FONT-SIZE: 8pt; COLOR: #323233; FONT-FAMILY: Tahoma, 'MS Sans Serif';
-}
-
-.bgr0 {
-	FONT-SIZE: 8pt; FONT-FAMILY: Tahoma, 'MS Sans Serif'; background-color: #b9c5d7;
-}
 
 table.list {
 	border-spacing: 1px;
@@ -585,7 +261,6 @@ table.list {
 	COLOR: #293869;
 
 	position: relative;
-	left: expression(this.parentElement.parentElement.parentElement.parentElement.scrollLeft);
 
 }
 .row-cell-transparent-no-scroll {
@@ -605,9 +280,8 @@ table.list {
 	COLOR: #293869;
 
 	position: relative;
-	left: expression(this.parentElement.parentElement.parentElement.parentElement.scrollLeft);
-
 }
+
 .row-cell-transparent {
 	FONT-FAMILY: Tahoma, 'MS Sans Serif';
 	FONT-WEIGHT: normal;
@@ -658,7 +332,6 @@ table.list {
 	padding-bottom: 5px;
 
 	position: relative;
-	left: expression(this.parentElement.parentElement.parentElement.parentElement.scrollLeft);
 
 }
 
@@ -685,16 +358,9 @@ table.list {
 	padding-top: 3px;
 	padding-bottom: 2px;
 
-	position: relative;
-	top: expression(this.parentElement.parentElement.parentElement.parentElement.scrollTop);
-
 	border-right:solid 1px #D6D3CE;
 	border-bottom:solid 1px #D6D3CE;
 	border-top:solid 1px #D6D3CE;
-
-	position: relative;
-	top: expression(this.parentElement.parentElement.parentElement.parentElement.scrollTop);
-	left: expression(this.parentElement.parentElement.parentElement.parentElement.scrollLeft);
 
 }
 
@@ -714,34 +380,7 @@ a.row-cell-header-a, a.row-cell-header-a:link, a.row-cell-header-a:hover, a.row-
 	background-color: #dededc;
 
 	position: relative;
-	top: expression(this.parentElement.parentElement.parentElement.parentElement.scrollTop);
-	left: expression(this.parentElement.parentElement.parentElement.parentElement.scrollLeft);
 
-}
-
-A.lnk0, A.lnk0:link, A.lnk0:active, A.lnk0:hover, A.lnk0:visited {
-	FONT-SIZE: 8pt;
-	COLOR: #000000;
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
-	TEXT-DECORATION: none;
-}
-
-A.lnk4, A.lnk4:link, A.lnk4:active, A.lnk4:hover, A.lnk4:visited {
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
-	FONT-WEIGHT: bold;
-	FONT-SIZE:  8pt;
-	COLOR: #000000;
-	TEXT-DECORATION: none
-}
-
-A.lnk15 {
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
-	FONT-SIZE: 10pt;
-	COLOR: #293869;
-	TEXT-DECORATION: none;
-	border:none;
-
-	background-color: transparent;
 }
 
 A.row-cell, A.row-cell:link, A.row-cell:active, A.row-cell:hover, A.row-cell:visited {
@@ -749,9 +388,9 @@ A.row-cell, A.row-cell:link, A.row-cell:active, A.row-cell:hover, A.row-cell:vis
 	FONT-SIZE: 10pt;
 	COLOR: #293869;
 	TEXT-DECORATION: none;
-      	margin: 0px;
-      	padding: 0px;
-      	border: 0px;
+	margin: 0px;
+	padding: 0px;
+	border: 0px;
 	background-color: transparent;
 }
 
@@ -768,23 +407,11 @@ A.row-button, A.row-button:link, A.row-button:active, A.row-button:hover, A.row-
 
 .form-active-label, .form-passive-label {
 	FONT-WEIGHT: bold;
-	FONT-SIZE:  8pt;
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
-	COLOR: #000000;
 	background-color: #ececec;
-
-	padding-left: 10px;
-	padding-right: 10px;
-	padding-top: 5px;
-	padding-bottom: 5px;
-
+	padding: 5px 10px;
 	border-color: #d6d3ce;
 	border-style:solid;
-
-	border-left-width: 0px;
-	border-top-width: 0px;
-	border-right-width: 1px;
-	border-bottom-width: 1px;
+	border-width: 0 1px 1px 0;
 }
 
 .form-active-banner, .form-passive-banner {
@@ -828,12 +455,6 @@ A.row-button, A.row-button:link, A.row-button:active, A.row-button:hover, A.row-
 	border-right-width: 1px;
 	border-bottom-width: 1px;
 }
-
-
-
-
-
-
 
 .form-deleted-label {
 	FONT-WEIGHT: bold;
@@ -917,32 +538,12 @@ A.form-deleted-deleted, A.form-deleted-deleted:link, A.form-deleted-deleted:hove
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 .form-inner {
 	FONT-SIZE:  8pt;
 	FONT-FAMILY: Tahoma, 'MS Sans Serif';
 }
 
 .form-active-inputs, .form-passive-inputs {
-	FONT-SIZE:  8pt;
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
 	background-color: #f9f9ff;
 	padding-left: 5px;
 	padding-right: 10px;
@@ -950,11 +551,9 @@ A.form-deleted-deleted, A.form-deleted-deleted:link, A.form-deleted-deleted:hove
 	border-color: #d6d3ce;
 	border-style:solid;
 
-	border-left-width: 0px;
-	border-top-width: 0px;
-	border-right-width: 1px;
-	border-bottom-width: 1px;
+	border-width: 0 1px 1px 0;
 }
+
 .form-active-deleted, .form-passive-deleted {
 	FONT-SIZE:  8pt;
 	FONT-FAMILY: Tahoma, 'MS Sans Serif';
@@ -966,8 +565,6 @@ A.form-deleted-deleted, A.form-deleted-deleted:link, A.form-deleted-deleted:hove
 }
 
 A.form-active-inputs, A.form-active-inputs:link, A.form-active-inputs:hover, A.form-active-inputs:visited, A.form-passive-inputs, A.form-passive-inputs:link, A.form-passive-inputs:hover, A.form-passive-inputs:visited{
-	FONT-SIZE:  8pt;
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
 	COLOR: #293869;
 	background-color: #f9f9ff;
 	padding-left: 0px;
@@ -1004,11 +601,6 @@ span.get_down_the_text_1 {
 	display: inline-block;
 	margin: 4px;
 	vertical-align: middle;
-}
-
-SELECT {
-	FONT-SIZE: 8pt;
-	FONT-FAMILY: Tahoma, 'MS Sans Serif';
 }
 
 input.form-active-inputs, select.form-active-inputs, textarea.form-active-inputs {
@@ -1068,27 +660,4 @@ a.hint {
 	FONT-FAMILY: Tahoma, 'MS Sans Serif';
 }
 
-
-#admin a {color:#000000;text-decoration:none;font-family: Tahoma, Verdana, sans-serif;font-size:8pt;font-style:normal}
-
-#Menu {position:absolute;top:0;left:0;z-index:100;display:none}
-#Menu .mm {background-color:#C4C7C9;padding:6px 5px 6px 9px;border-bottom:solid 1px #E2E3E4;}
-#Menu .mm0 {background-color:#C4C7C9;padding:6px 5px 0px 9px;}
-#Menu a {color:#23385A;text-decoration:none;font-family: Tahoma, Verdana, sans-serif;font-size:8pt;font-style:normal}
-#Menu a:hover {color:#23385A;text-decoration:underline;font-family: Tahoma, Verdana, sans-serif;font-size:8pt;font-style:normal}
-
-div.grey-submit a {color:#222323;text-decoration:none;}
-div.grey-submit a:hover {color:#222323;text-decoration:underline;}
-
-a.grey-submit {color:#222323;text-decoration:none;FONT-SIZE: 8pt;}
-a.grey-submit:hover {color:#222323;text-decoration:underline;FONT-SIZE: 8pt;}
-
-.logon {
-	font-family: Tahoma, Verdana, sans-serif;
-	font-size:8pt;
-	font-style:normal;
-	font-weight: normal;
-	color:#414141;
-	text-decoration: none;
-}
 
