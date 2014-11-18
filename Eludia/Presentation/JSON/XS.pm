531acc7c9590ce1ace65bb18ed38a9d41f8bc265
Pavel Koudriavtsev <pik@cheby.ru>
2014-11-18 11:13:45 +0300

    CP1251 fixes

--- Eludia/Presentation/JSON/XS.pm
+++ Eludia/Presentation/JSON/XS.pm
@@ -4,7 +4,9 @@ use JSON::XS;
 
 sub setup_json {
 
-	our $_JSON = JSON::XS -> new -> allow_nonref (1) -> allow_blessed (1);
+	our $_JSON = $i18n -> {_charset} eq 'UTF-8' ?
+		JSON::XS -> new -> allow_nonref (1) -> allow_blessed (1) :
+		JSON::XS -> new -> latin1 (1) -> allow_nonref (1);
 
 }
 
