4c2895172f7da557fe283d326c6d24483ed3337e
Roman Lobzin <roman@cheby.ru>
2014-12-11 12:45:18 +0300

    _info: get branch name and last dt for Windows

--- Eludia/GenericApplication/Content/_info.pm
+++ Eludia/GenericApplication/Content/_info.pm
@@ -5,18 +5,18 @@ sub select__info {
 	my $os_name = $^O;
 	my $os_version;
 
-	if ($^O eq 'MSWin32') {		
-	
+	if ($^O eq 'MSWin32') {
+
 		eval {
-		
+
 			require Win32;
-			
+
 			my ($string, $major, $minor, $build, $id) = Win32::GetOSVersion ();
-			
+
 			$os_name    = 'MS Windows';
-			
+
 			$os_version = {
-			
+
 				140  => '95',
 				1410 => '98',
 				1490 => 'Me',
@@ -26,27 +26,28 @@ sub select__info {
 				251  => 'XP',
 				252  => '2003',
 				260  => 'Vista',
-				
+
 			} -> {$id . $major . $minor} . " Build $build";
-			
-		};	
-		
-	} 
+
+		};
+
+	}
 	else {
-	
+
 		eval {
 
 			require POSIX;
 
 			($os_name, my $nodename, $os_version, my $version, my $machine) = POSIX::uname ();
 
-		};	
-		
+		};
+
 	}
-	
+
 	setup_skin ();
-	
-	my $git_path = `which git`; chomp $git_path;
+
+	my $git_path = $^O eq 'MSWin32' ? '"C:\Program Files (x86)\Git\bin\git.exe"' : `which git`;
+	chomp $git_path;
 
 	my $application_package = "";
 
@@ -56,13 +57,14 @@ sub select__info {
 
 		$library_path =~ s/\/GenericApplication//;
 
-		if ( -x "$git_path" && -d "$library_path/../.git" ) {
+		if ( (-x "$git_path" || $^O eq 'MSWin32') && -d "$library_path/../.git" ) {
 
 			my $branch = `$git_path --git-dir=$library_path/../.git rev-parse --abbrev-ref HEAD`;
 			chomp $branch;
 
-			my $dt_last_commit = `$git_path --git-dir=$library_path/../.git log -1 --format=%cd`;
-			chomp $dt_last_commit;
+			my $last_commit = `$git_path --git-dir=$library_path/../.git log -1`;
+			$last_commit =~ /\nDate:\s+([^\n]+)/gsm;
+			$dt_last_commit = $1;
 
 			$application_package .= " ($branch, $dt_last_commit)";
 
@@ -73,25 +75,25 @@ sub select__info {
 	}
 
 	my $data = {rows => [
-	
+
 		{
-			id    => 'OS',
-			label => $os_name,
+			id      => 'OS',
+			label   => $os_name,
 			version => $os_version,
 		},
 
 		{
-			
-			id    => 'WEB server',
-			label => $ENV {SERVER_SOFTWARE},
-		
-		},	
+
+			id      => 'WEB server',
+			label   => $ENV {SERVER_SOFTWARE},
+
+		},
 
 		{
-			id    => 'Interpreter',
-			label => (sprintf "Perl %vd", $^V),
+			id      => 'Interpreter',
+			label   => (sprintf "Perl %vd", $^V),
 		},
-	
+
 		{
 			id      => 'DBMS',
 			label   => $SQL_VERSION -> {string},
@@ -100,81 +102,81 @@ sub select__info {
 		},
 
 		{
-			id    => 'DB interface',
-			label => 'DBI ' . $DBI::VERSION,
+			id      => 'DB interface',
+			label   => 'DBI ' . $DBI::VERSION,
 		},
 
 		{
-			id    => 'DB driver',
-			label => 'DBD::' . $db -> {Driver} -> {Name} . ' ' . ${'DBD::' . $db -> {Driver} -> {Name} . '::VERSION'},
+			id      => 'DB driver',
+			label   => 'DBD::' . $db -> {Driver} -> {Name} . ' ' . ${'DBD::' . $db -> {Driver} -> {Name} . '::VERSION'},
 		},
-		
-		{			
-			id    => 'Parameters module',
-			label => ref $apr,
+
+		{
+			id      => 'Parameters module',
+			label   => ref $apr,
 			version => ${(ref $apr) . '::VERSION'},
 		},
-		
-		{			
-			id    => 'Engine',
-			label => "Eludia",
-			path  => $preconf -> {core_path},
+
+		{
+			id      => 'Engine',
+			label   => "Eludia",
+			path    => $preconf -> {core_path},
 			version => $Eludia::VERSION,
 		},
 
 		{
-			id    => 'JSON module',
-			label => ref $_JSON,
+			id      => 'JSON module',
+			label   => ref $_JSON,
 			version => ${(ref $_JSON) . '::VERSION'},
 		},
 
-		{			
-			id    => 'Application package',
-			label => ($_PACKAGE =~ /(\w+)/),
-			path  => $application_package,
+		{
+			id      => 'Application package',
+			label   => ($_PACKAGE =~ /(\w+)/),
+			path    => $application_package,
 		},
 
 		{
-			id    => 'Skin',
-			path  => $INC {$_SKIN},
-			label => $_SKIN
+			id      => 'Skin',
+			path    => $INC {$_SKIN},
+			label   => $_SKIN
 		},
 
 	]};
-	
+
 	my $exe = $^X;
-	
+
 	$data -> {rows} -> [$exe =~ /perl(\.\w+)?/ ? 2 : 1] -> {path} = $exe;
-	
+
 	foreach my $i (@{$data -> {rows}}) {
-	
+
 		unless ($i -> {path}) {
-		
+
 			my ($key) = split / /, $i -> {label};
-			
+
 			$key =~ s{\:\:}{\/}g;
-			
+
 			$i -> {path} = $INC {$key . '.pm'};
-		
+
 		}
-		
+
 		if ($i -> {version}) {
-		
+
 			$i -> {product} = $i -> {label};
-		
+
 		}
 		else {
-	
+
 			($i -> {product}, $i -> {version}) = split m{[ /]}, $i -> {label};
-	
+
 		}
-		
+
 		$i -> {product} =~ s{^Eludia::Presentation::Skins::}{};
-						
+
 	}
 
 	return $data;
-		
+
 
 }
 
