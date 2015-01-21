018a87cd8553c07bc415549255472814ce243b53
Roman Lobzin <roman@cheby.ru>
2015-01-21 17:16:01 +0300

    lrt - print time on each line

--- Eludia/Presentation/Skins/TurboMilk.pm
+++ Eludia/Presentation/Skins/TurboMilk.pm
@@ -3741,9 +3741,17 @@ sub lrt_print {
 
 	my $_SKIN = shift;
 
+	my $time = '';
+	unless ($_REQUEST {__lrt_no_time}) {
+		my $sec = int (time() - $_REQUEST {__lrt_time});
+		my $min = int ($sec / 60);
+		$sec -= $min * 60;
+		$time = sprintf ("%02d:%02d - ", $min, $sec);
+	}
+
 	my $id = int (time * rand);
 	$r -> print ("<span id='$id'><font color=white>");
-	$r -> print (@_);
+	$r -> print ($time, @_);
 	$r -> print ("</span>");
 	$r -> print ($lrt_bar);
 	$r -> print (<<EOH);
@@ -3752,7 +3760,6 @@ sub lrt_print {
 	</script>
 	</body></html>
 EOH
-
 
 }
 
@@ -3772,6 +3779,7 @@ sub lrt_ok {
 	my $_SKIN = shift;
 	my $color = $_[1] ? 'red' : 'yellow';
 	my $label = $_[1] ? 'Ошибка' : 'ОК';
+	local $_REQUEST {__lrt_no_time} = 1;
 	$_SKIN -> lrt_println ("$_[0] <font color='$color'><b>[$label]</b></font>");
 }
 
@@ -3786,11 +3794,15 @@ sub lrt_start {
 	$r -> content_type ('text/html; charset=windows-1251');
 	$r -> send_http_header ();
 
+	$_REQUEST {__lrt_time} = time();
+
+	local $_REQUEST {__lrt_no_time} = 1;
 	$_SKIN -> lrt_print (<<EOH);
 		<html><head><LINK href="$_REQUEST{__static_url}/eludia.css?$_REQUEST{__static_salt}" type="text/css" rel="STYLESHEET"><style>BODY {background-color: black}</style></head><BODY BGCOLOR='#000000' TEXT='#dddddd'><font face='Courier New'>
 			<iframe name=invisible src="$_REQUEST{__static_url}/0.html" width=0 height=0 application="yes">
 			</iframe>
 EOH
+	$_SKIN -> lrt_println ('Время - Сообщение');
 
 }
 
@@ -3820,6 +3832,7 @@ sub lrt_finish {
 
 	}
 	elsif ($options -> {kind} eq 'link') {
+		local $_REQUEST {__lrt_no_time} = 1;
 		$_SKIN -> lrt_print (<<EOH);
 		<br><a href="javascript: document.location = '$href'"><font color='yellow'>$banner</font></a>
 		</body></html>
