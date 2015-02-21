660272d9538ec16839c357ea6251172be0a74bbe
Pavel Koudriavtsev <pik@cheby.ru>
2015-02-21 13:20:47 +0300

    Mint: table editing performance optimizations

--- Eludia/Presentation/Skins/Mint.pm
+++ Eludia/Presentation/Skins/Mint.pm
@@ -1772,9 +1772,8 @@ sub draw_toolbar_input_combo {
 
 	my ($values, $placeholder);
 	foreach my $value (@{$options -> {values}}) {
-		if (!$value -> {id}) {
+		if ($value -> {empty}) {
 			$placeholder = $value -> {label};
-			push @$values, $value;
 		} elsif ($value -> {id} != -1) {
 			push @$values, $value;
 		}
