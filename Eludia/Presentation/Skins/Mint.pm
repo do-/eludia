d8c728571b374b3c0cc7959bdf83da4d82676abb
Aleksandr Dmitriev <admitriev@cheby.ru>
2017-08-29 11:53:08 +0300

    Wrap top toolbar on div with table id

--- Eludia/Presentation/Skins/Mint.pm
+++ Eludia/Presentation/Skins/Mint.pm
@@ -3046,13 +3046,15 @@ EOJS
 
 		$$options{title}
 		$$options{path}
-		$$options{top_toolbar}
+		<div data-st-id="$$options{id_table}">
+			$$options{top_toolbar}
+		</div>
 
 		<form name="$$options{name}" action="$_REQUEST{__uri}" method="post" target="invisible" $enctype>
-		<input type=hidden name="__suggest" value="" />
-		$hiddens_html
-		$html
-		$$options{toolbar}
+			<input type=hidden name="__suggest" value="" />
+			$hiddens_html
+			$html
+			$$options{toolbar}
 		</form>
 EOH
 
